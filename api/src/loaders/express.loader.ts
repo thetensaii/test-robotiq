56bb39c482095d57af027e684e22e72d5ed0c395
Fofana Elhadji Salimou <sfofana@atecna.ca>
2022-04-03 20:25:03 -0400

    api/fix : Add cors

--- api/src/loaders/express.loader.ts
+++ api/src/loaders/express.loader.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { ApiRouter } from '../routes/api.route';
+import cors from 'cors';
 
 export default async ({ app }: { app: express.Application }): Promise<void> => {
+	app.use(cors());
+
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
 
